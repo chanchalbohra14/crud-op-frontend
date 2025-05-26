a0f806ddd7ecc10934439985264d3f0cb228435c
chanchalbohra <chanchalbohra14@gmail.com>
2025-05-26 17:35:22 +0530

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import MemberForm from "./Components/MemberForm";
-import GetMembers from "./Components/GetMembers";
-import GetMember from "./Components/GetMember";
-import EditMember from "./Components/EditMember";
-
+import MemberForm from "./Components/MemberForm/MemberForm";
+import GetMembers from "./Components/GetMembers/GetMembers";
+import GetMember from "./Components/GetMember/GetMember";
+import EditMember from "./Components/EditMember/EditMember";
 const App = () => {
   return (
     <div>
